26984b0bfb30b746b48e01fb31787e55030377f2
Wen-Chun Lin <lovecankill@gmail.com>
2017-04-26 15:24:39 +0800

    Remove unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,14 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Page1Component } from './page1/page1.component';
-import { Page2Component } from './page2/page2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { LoginComponent } from './login/login.component';
 import { CardsComponent } from './cards/cards.component';
 import { FlotComponent } from './charts/flot/flot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Page1Component,
-    Page2Component,
     DashboardComponent,
-    LoginComponent,
     CardsComponent,
     FlotComponent
   ],
