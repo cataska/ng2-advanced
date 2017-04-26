7cd2778fbc0bd30f276e6f2e67c83b3ba7013877
Wen-Chun Lin <lovecankill@gmail.com>
2017-04-26 10:53:58 +0800

    Add login and dashboard components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Page1Component,
-    Page2Component
+    Page2Component,
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
