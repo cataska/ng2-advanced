0b38a07c4050ea535f17c3f517b3c4db3a7a55b6
Wen-Chun Lin <lovecankill@gmail.com>
2017-04-26 16:52:38 +0800

    Use layout and add login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,39 @@
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
 import { CardsComponent } from './cards/cards.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { fallbackRoute } from './shared/fallback-route';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'dashboard', pathMatch: 'full'
+  { path: '', component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard', pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'cards/:type',
+        component: CardsComponent
+      },
+      {
+        path: 'charts',
+        loadChildren: './charts/charts.module#ChartsModule'
+      }
+    ]
   },
-  {
-    path: 'dashboard',
-    component: DashboardComponent
-  },
-  {
-    path: 'cards/:type',
-    component: CardsComponent
-  },
-  {
-    path: 'charts',
-    loadChildren: './charts/charts.module#ChartsModule'
-  }
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    useHash: true
+    useHash: true,
+    preloadingStrategy: PreloadAllModules
   })],
   exports: [RouterModule]
 })
