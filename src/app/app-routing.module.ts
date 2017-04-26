b6d688ff8ff66bcbcaf88fbfee9993a464c494d1
Wen-Chun Lin <lovecankill@gmail.com>
2017-04-26 10:25:15 +0800

    * Redirect route
    * Wildcard route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { fallbackRoute } from './shared/fallback-route';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -6,13 +7,17 @@ import { Page2Component } from './page2/page2.component';
 
 const routes: Routes = [
   {
+    path: '', redirectTo: 'page1', pathMatch: 'full'
+  },
+  {
     path: 'page1',
     component: Page1Component
   },
   {
     path: 'page2',
     component: Page2Component
-  }
+  },
+  fallbackRoute
 ];
 
 @NgModule({
